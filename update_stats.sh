f951c2cfa4ce96c2bace2570ca84a2de23855b5f
Jos Dehaes <jos.dehaes@gmail.com>
2025-03-09 19:14:36 +0100

    use OS name and not tag for grouping
    Signed-off-by: Jos Dehaes <jos.dehaes@gmail.com>

--- update_stats.sh
+++ update_stats.sh
@@ -30,14 +30,14 @@ echo "Note: Devices with only a few installs are development tests (or people ha
 
 echo
 
-echo "Breakdown by installer tag:"
+echo "Breakdown by installed OS:"
 
 cat <<EOF | su - postgres -s /bin/sh -c "psql asahistats"
 select
     count(*) as count,
-    data#>>Array['installer','tag'] as tag
+    trim(regexp_replace(data->>'os_name', '([^a-zA-Z])\(?[0-9]+\)? ?([^a-zA-Z]|$)', '\1\3', 'g')) as os
 from stats
-group by tag
+group by os
 order by count desc;
 EOF
 
